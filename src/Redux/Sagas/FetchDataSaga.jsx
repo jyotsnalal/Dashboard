a1c0f0ae3d666564a8307a061c3c4c6d459251fd
jyotsnalal <71563166+jyotsnalal@users.noreply.github.com>
2024-08-18 23:37:12 +0530

    Update FetchDataSaga.jsx

--- src/Redux/Sagas/FetchDataSaga.jsx
+++ src/Redux/Sagas/FetchDataSaga.jsx
@@ -1,10 +1,11 @@
+
 // src/redux/sagas/fetchDataSaga.js
 import { call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 import { FETCH_DATA_REQUEST, fetchDataSuccess, fetchDataFailure } from '../Action/Index';
 
 function fetchUserActivitiesAPI() {
-  return axios.get('http://52.168.1.54:8080/api/v1/userActivities');
+  return axios.get('api/proxy');
 }
 
 
